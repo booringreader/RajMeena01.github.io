06d537232bcb28378df2dd3484d3dac90cd0f599
Raj Meena <heyrajmeena@gmail.com>
2024-09-22 19:30:26 +0530

    build configs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/RajMeena01.github.io/"
+  plugins: [react()]
 })
